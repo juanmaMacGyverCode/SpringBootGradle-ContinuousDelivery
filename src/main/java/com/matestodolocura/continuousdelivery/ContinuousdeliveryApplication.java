7e820fef972262601fc28282b56042d37d4eb1fa
Juanma <jmlpdr.93@gmail.com>
2021-02-06 17:01:08 +0000

    first commit

---
+++ src/main/java/com/matestodolocura/continuousdelivery/ContinuousdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.matestodolocura.continuousdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousdeliveryApplication.class, args);
+	}
+
+}
